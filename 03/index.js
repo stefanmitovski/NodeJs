58574ca69a555aa453f48501b12b2152760b8192
Semos Akademija <akademija@semosedu.local>
2023-02-20 21:31:26 +0100

    chas3

---
+++ 03/index.js
@@ -0,0 +1,6 @@
+// const { b, c } = require('./text_convert');
+// console.log(b);
+// console.log(c);
+
+const test_funkcija=require('./text_convert');
+ 
---
+++ 03/text_convert.js
@@ -0,0 +1,13 @@
+// const b = 25;
+// const c = 'zdravo';
+
+const test_funkcija =(x,y)=>{
+
+}
+
+// module.exports = {
+//     b,
+//     c
+// }
+
+module.export = test_funkcija;
