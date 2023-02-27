3db41762f3f868e83290d0e02545d8c7fc97abcc
Semos Akademija <akademija@semosedu.local>
2023-02-27 21:33:50 +0100

    chas4

---
+++ 04/index.js
@@ -0,0 +1,9 @@
+// console.log('Ova e mojot cetvrt cas od Node.js');
+const path = require('path');
+
+console.log(__filename);
+console.log(path.basename(__filename));
+console.log(__filename.split("\\")[__filename.split("\\").length-1]);
+
+//Direcotory name
+
